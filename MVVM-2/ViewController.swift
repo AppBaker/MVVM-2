30f8d13c9b587e0de264d5c3a82be8345387a85d
IvanNikitin <nqqaqq@gmail.com>
2019-12-16 11:36:56 +0700

    Initial Commit

---
+++ MVVM-2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM-2
+//
+//  Created by Ivan Nikitin on 16.12.2019.
+//  Copyright © 2019 Ivan Nikitin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
